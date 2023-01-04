11db9457de89b48bee54f7aa250d8f76a2e1a9f5
Aleks <44847900+agredyaev@users.noreply.github.com>
2023-01-04 14:30:56 +0100

    added s7-lessons

---
+++ s7-lessons/Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 1/realization.py
@@ -0,0 +1,12 @@
+import findspark
+findspark.init()
+findspark.find()
+import os
+os.environ['HADOOP_CONF_DIR'] = '/etc/hadoop/conf'
+os.environ['YARN_CONF_DIR'] = '/etc/hadoop/conf' 
+
+
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession.builder.config("spark.executor.memory", "1g").config("spark.driver.memory", "1g").config("spark.executor.cores", 2).config("spark.driver.cores", 2).appName("My first session").getOrCreate()
