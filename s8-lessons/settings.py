6dc2a57950bb5026baf7ff150dad680fd3c5614c
Aleksey <aleksey.gredyaev@gmail.com>
2023-02-03 21:58:21 +0100

    add 3.5.1

--- s8-lessons/settings.py
+++ s8-lessons/settings.py
@@ -1,2 +1,2 @@
-USER_HOST = '51.250.83.133' # укажите свой хост
-TOPIC_NAME = 'student.topic.cohort<5>.<agredyaev>' # укажите название топика student.topic.cohort<номер когорты>.<username>
+USER_HOST = '130.193.49.24' # укажите свой хост
+TOPIC_NAME = 'student.topic.cohort5.agredyaev' # укажите название топика student.topic.cohort<номер когорты>.<username>
--- s8-lessons/Тема 3. Настройка потока данных/5. Преобразование потоковых данных/Задание 1/realization.py
+++ s8-lessons/Тема 3. Настройка потока данных/5. Преобразование потоковых данных/Задание 1/realization.py
@@ -1,30 +1,57 @@
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql import functions as f
+from pyspark.sql import functions as F
 from pyspark.sql.types import StructType, StructField, DoubleType, StringType, TimestampType
+from settings import TOPIC_NAME
+
+
+print(TOPIC_NAME)
 
 # необходимая библиотека с идентификатором в maven
 # вы можете использовать ее с помощью метода .config и опции "spark.jars.packages"
-kafka_lib_id =
-        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
+spark_jars_packages = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
 
 # настройки security для кафки
 # вы можете использовать из с помощью метода .options(**kafka_security_options)
-kafka_security_options = {
+config = {
+    'kafka.bootstrap.servers': 'rc1b-2erh7b35n4j4v869.mdb.yandexcloud.net:9091',
     'kafka.security.protocol': 'SASL_SSL',
     'kafka.sasl.mechanism': 'SCRAM-SHA-512',
-    'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username=\"de-student\" password=\"ltcneltyn\";',
+    'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username="de-student" password="ltcneltyn";',
+    "subscribe": TOPIC_NAME
 }
 
+
 def spark_init() -> SparkSession:
-		pass
+    return SparkSession.builder \
+        .appName("read kafka topic") \
+        .config("spark.jars.packages", spark_jars_packages) \
+        .getOrCreate()
 
 
 def load_df(spark: SparkSession) -> DataFrame:
-		pass # не забудьте здесь указать то-же имя топика что и в TOPIC_NAME 
+    return spark.readStream \
+        .format('kafka') \
+        .options(**config) \
+        .load()
 
 
 def transform(df: DataFrame) -> DataFrame:
-    pass
+
+    schema = StructType([
+
+        StructField("lat", DoubleType()),
+        StructField("timestamp", TimestampType()),
+        StructField("lon", DoubleType()),
+        StructField("client_id", StringType()),
+    ])
+
+    return df \
+        .withColumn('key', F.col('key').cast(StringType())) \
+        .withColumn('value', F.col('value').cast(StringType())) \
+        .withColumn('event', F.from_json('value', schema=schema)) \
+        .select('event.*', '*') \
+        .withColumn('timestamp', F.from_unixtime(F.col('timestamp'), "yyyy-MM-dd' 'HH:mm:ss.SSS").cast(TimestampType())) \
+        .drop('event')
 
 
 spark = spark_init()
@@ -33,12 +60,13 @@ source_df = load_df(spark)
 output_df = transform(source_df)
 
 query = (output_df
-         .writeStream
-         .outputMode("append")
-         .format("console")
-         .option("truncate", False)
-         .trigger(once=True)
-         .start())
+            .writeStream
+            .outputMode("append")
+            .format("console")
+            .option("truncate", False)
+            .trigger(once=True)
+            .start()
+            )
 try:
     query.awaitTermination()
 finally:
