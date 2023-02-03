254813d197b872ba548d2a55a9583473005c2478
Aleksey <aleksey.gredyaev@gmail.com>
2023-02-03 21:13:15 +0100

    add 3.4.2

--- s8-lessons/Тема 3. Настройка потока данных/4. Изучение данных в Kafka со Spark/Задание 2/realization.py
+++ s8-lessons/Тема 3. Настройка потока данных/4. Изучение данных в Kafka со Spark/Задание 2/realization.py
@@ -1,30 +1,51 @@
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as f, DataFrame
+from pyspark.sql import functions as F, DataFrame
 from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType
 
 # необходимая библиотека с идентификатором в maven
 # вы можете использовать ее с помощью метода .config и опции "spark.jars.packages"
-kafka_lib_id =
-        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
+spark_jars_packages = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
 
 # настройки security для кафки
 # вы можете использовать из с помощью метода .options(**kafka_security_options)
-kafka_security_options = {
+config = {
+    'kafka.bootstrap.servers': 'rc1b-2erh7b35n4j4v869.mdb.yandexcloud.net:9091',
     'kafka.security.protocol': 'SASL_SSL',
     'kafka.sasl.mechanism': 'SCRAM-SHA-512',
-    'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username=\"de-student\" password=\"ltcneltyn\";',
+    'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username="de-student" password="ltcneltyn";',
+    "subscribe": 'persist_topic'
 }
 
+
 def spark_init() -> SparkSession:
-    pass
+    return SparkSession.builder \
+        .appName("read kafka topic") \
+        .config("spark.jars.packages", spark_jars_packages) \
+        .getOrCreate()
 
 
 def load_df(spark: SparkSession) -> DataFrame:
-    pass
+    return spark.read \
+        .format('kafka') \
+        .options(**config) \
+        .load()
 
 
 def transform(df: DataFrame) -> DataFrame:
-    pass
+    schema = StructType([
+        StructField("subscription_id", IntegerType()),
+        StructField("name", StringType()),
+        StructField("description", StringType()),
+        StructField("price", DoubleType()),
+        StructField("currency", StringType()),
+    ])
+
+    return df \
+        .withColumn('key', F.col('key').cast(StringType())) \
+        .withColumn('value', F.col('value').cast(StringType())) \
+        .withColumn('event', F.from_json('value', schema=schema)) \
+        .select('event.*', '*') \
+        .drop('event')
 
 
 spark = spark_init()
