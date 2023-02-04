1f630d05d9787d919ed4858bf98cc89baf3f8529
Aleksey <aleksey.gredyaev@gmail.com>
2023-02-04 09:25:47 +0000

    final 3.5.1

--- s8-lessons/Тема 3. Настройка потока данных/5. Преобразование потоковых данных/Задание 1/realization.py
+++ s8-lessons/Тема 3. Настройка потока данных/5. Преобразование потоковых данных/Задание 1/realization.py
@@ -1,20 +1,16 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql import functions as F
-from pyspark.sql.types import StructType, StructField, DoubleType, StringType, TimestampType
-from settings import TOPIC_NAME
+from pyspark.sql.types import StructType, StructField, DoubleType, StringType, TimestampType, LongType
 
-# необходимая библиотека с идентификатором в maven
-# вы можете использовать ее с помощью метода .config и опции "spark.jars.packages"
+
 spark_jars_packages = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
 
-# настройки security для кафки
-# вы можете использовать из с помощью метода .options(**kafka_security_options)
 config = {
     'kafka.bootstrap.servers': 'rc1b-2erh7b35n4j4v869.mdb.yandexcloud.net:9091',
     'kafka.security.protocol': 'SASL_SSL',
     'kafka.sasl.mechanism': 'SCRAM-SHA-512',
     'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username="de-student" password="ltcneltyn";',
-    "subscribe": TOPIC_NAME
+    "subscribe": 'student.topic.cohort5.agredyaev'
 }
 
 
@@ -37,18 +33,17 @@ def transform(df: DataFrame) -> DataFrame:
     schema = StructType([
 
         StructField("lat", DoubleType()),
-        StructField("timestamp", TimestampType()),
+        StructField("timestamp", DoubleType()),
         StructField("lon", DoubleType()),
-        StructField("client_id", StringType()),
+        StructField("client_id", StringType())
     ])
 
     return df \
         .withColumn('key', F.col('key').cast(StringType())) \
         .withColumn('value', F.col('value').cast(StringType())) \
         .withColumn('event', F.from_json('value', schema=schema)) \
-        .select('event.*', '*') \
-        .withColumn('timestamp', F.from_unixtime(F.col('timestamp'), "yyyy-MM-dd' 'HH:mm:ss.SSS").cast(TimestampType())) \
-        .drop('event')
+        .select('event.*') \
+        .withColumn('timestamp', F.from_unixtime(F.col('timestamp'), "yyyy-MM-dd' 'HH:mm:ss.SSS").cast(TimestampType()))
 
 
 def write_stream(df):
@@ -59,10 +54,10 @@ def write_stream(df):
         .option("truncate", False) \
         .trigger(once=True) \
         .start()
- 
+
+
 
 spark = spark_init()
-
 source_df = load_df(spark)
 output_df = transform(source_df)
 
