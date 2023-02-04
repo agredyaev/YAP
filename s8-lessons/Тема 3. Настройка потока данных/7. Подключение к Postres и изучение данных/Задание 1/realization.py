393965914d3e650794be4a459f254bccd3d7232d
Aleksey <aleksey.gredyaev@gmail.com>
2023-02-04 11:29:25 +0000

    add 3.7.1

--- s8-lessons/Тема 3. Настройка потока данных/7. Подключение к Postres и изучение данных/Задание 1/realization.py
+++ s8-lessons/Тема 3. Настройка потока данных/7. Подключение к Postres и изучение данных/Задание 1/realization.py
@@ -1,10 +1,33 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 
-spark = (
-        #
-    )
+spark_jar_packages = ','.join([
+    "org.postgresql:postgresql:42.4.0"
+])
 
-df = (spark.read
-        #
-        .load())
+postgres_config = {
+    'url': 'jdbc:postgresql://rc1a-fswjkpli01zafgjm.mdb.yandexcloud.net:6432/de',
+    'driver': 'org.postgresql.Driver',
+    'schema': 'public',
+    'dbtable': 'marketing_companies',
+    'user': 'student',
+    'password': 'de-student'
+}
+
+def session_init() -> SparkSession:
+    return SparkSession.builder \
+        .appName("join data") \
+        .config("spark.jars.packages", spark_jar_packages) \
+        .getOrCreate()
+
+
+def read_postgres(spark: SparkSession) -> DataFrame:
+        return spark \
+                .read \
+                .format('jdbc') \
+                .options(**postgres_config) \
+                .load()
+
+
+spark = session_init()
+df = read_postgres(spark=spark)
 df.count()
