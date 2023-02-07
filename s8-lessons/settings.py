8b1064f971a8efde63849e2b3fd27c01e64a6f88
Aleksey <aleksey.gredyaev@gmail.com>
2023-02-07 18:38:56 +0000

    change settings

--- s8-lessons/settings.py
+++ s8-lessons/settings.py
@@ -1,2 +1,2 @@
-USER_HOST = '158.160.60.156' # укажите свой хост
+USER_HOST = '62.84.124.27' # укажите свой хост
 TOPIC_NAME = 'student.topic.cohort5.agredyaev' # укажите название топика student.topic.cohort<номер когорты>.<username>
