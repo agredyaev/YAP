1f6a3a71dcd6b600183850d01054b2f0842e05ef
Aleksey Gredyaev <aleksey.gredyaev@gmail.com>
2023-02-18 14:57:31 +0100

    add create cdm.user_product_counters

---
+++ s9-lessons/Тема 5. Написание первого сервиса/2. Проектируем структуру БД/Задание 2/create_cmd_user_product_counters.sql
@@ -0,0 +1,10 @@
+CREATE TABLE cdm.user_product_counters (
+	id serial NOT NULL,
+	user_id uuid NOT NULL,
+	product_id uuid NOT NULL,
+	product_name varchar NOT NULL,
+	order_cnt int NOT NULL,
+	CONSTRAINT user_product_counters_check CHECK ((order_cnt >= 0)),
+	CONSTRAINT user_product_counters_pk PRIMARY KEY (id)
+);
+CREATE UNIQUE INDEX user_product_counters_user_id_idx ON cdm.user_product_counters USING btree (user_id, product_id);
