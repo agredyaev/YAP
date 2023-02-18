141a3c65ae6c4d24a21a3465d9f52b294f3d5c72
Aleksey Gredyaev <aleksey.gredyaev@gmail.com>
2023-02-18 16:58:35 +0100

    change consumer and produser

--- s9-lessons/Тема 3. Подготовка и изучение источников/6. Изучение данных в Kafka/producer.py
+++ s9-lessons/Тема 3. Подготовка и изучение источников/6. Изучение данных в Kafka/producer.py
@@ -1,21 +1,28 @@
 from confluent_kafka import Producer
 import os
 
-TOPIC_NAME = 'order-service_orders'
+TOPIC_NAME = "order-service_orders"
+KAFKA_HOST = os.environ["YAP_KAFKA_HOST"]
+KAFKA_PORT = 9091
+
 
 def error_callback(err):
-    print('Something went wrong: {}'.format(err))
+    print("Something went wrong: {}".format(err))
+
 
 params = {
-    'bootstrap.servers': 'rc1a-d9i1f6b4scqrnsl9.mdb.yandexcloud.net:9091',
-    'security.protocol': 'SASL_SSL',
-    'ssl.ca.location': '/usr/local/share/ca-certificates/Yandex/YandexInternalRootCA.crt',
-    'sasl.mechanism': 'SCRAM-SHA-512',
-    'sasl.username': 'producer_consumer',
-    'sasl.password': os.environ['YAP_KAFKA_PASS'],
-    'error_cb': error_callback,
+    "bootstrap.servers": f"{KAFKA_HOST}:{KAFKA_PORT}",
+    "security.protocol": "SASL_SSL",
+    "ssl.ca.location": "/usr/local/share/ca-certificates/Yandex/YandexInternalRootCA.crt",
+    "sasl.mechanism": "SCRAM-SHA-512",
+    "sasl.username": "producer_consumer",
+    "sasl.password": os.environ["YAP_KAFKA_PASS"],
+    "error_cb": error_callback,
 }
 
 p = Producer(params)
-p.produce(TOPIC_NAME, '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+p.produce(
+    TOPIC_NAME,
+    ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",
+)
 # p.flush(10)
