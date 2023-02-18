fbd5f03a77b83cf0a577d79e69a3fd2ac223aec9
Aleksey Gredyaev <aleksey.gredyaev@gmail.com>
2023-02-18 16:24:49 +0100

    add create lind order to user

--- s9-lessons/Тема 5. Написание первого сервиса/2. Проектируем структуру БД/Задание 13/create_dds_l_order_user.sql
+++ s9-lessons/Тема 5. Написание первого сервиса/2. Проектируем структуру БД/Задание 13/create_dds_l_order_user.sql
@@ -1,12 +1,11 @@
-CREATE TABLE IF NOT EXISTS dds.l_product_category (
-	hk_product_category_pk uuid NOT NULL,
-	h_product_pk uuid NOT NULL,
-	h_category_pk uuid NOT NULL,
+CREATE TABLE dds.l_order_user (
+	hk_order_user_pk uuid NOT NULL,
+	h_order_pk uuid NOT NULL,
+	h_user_pk uuid NOT NULL,
 	load_dt timestamp NOT NULL,
 	load_src varchar NOT NULL,
-	CONSTRAINT l_product_category_pk PRIMARY KEY (hk_product_category_pk)
+	CONSTRAINT l_order_user_pk PRIMARY KEY (hk_order_user_pk)
 );
 
-
-ALTER TABLE dds.l_product_category ADD CONSTRAINT l_product_category_fk_h_category FOREIGN KEY (h_category_pk) REFERENCES dds.h_category(h_category_pk) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE dds.l_product_category ADD CONSTRAINT l_product_category_fk_h_product FOREIGN KEY (h_product_pk) REFERENCES dds.h_product(h_product_pk) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE dds.l_order_user ADD CONSTRAINT l_order_user_fk_h_order FOREIGN KEY (h_order_pk) REFERENCES dds.h_order(h_order_pk) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE dds.l_order_user ADD CONSTRAINT l_order_user_fk_h_user FOREIGN KEY (h_user_pk) REFERENCES dds.h_user(h_user_pk) ON DELETE CASCADE ON UPDATE CASCADE;
