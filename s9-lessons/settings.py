dfcf80d99f36cd8fa26e3649afe451bc7160eda8
Aleksey Gredyaev <aleksey.gredyaev@gmail.com>
2023-02-18 13:03:13 +0100

    update settings

--- s9-lessons/settings.py
+++ s9-lessons/settings.py
@@ -1,14 +1,21 @@
+import os
+
+dbuser = os.environ["YAP_POSTGRES_LOGIN"]
+password = os.environ["YAP_POSTGRES_PASS"]
+host = os.environ["YAP_POSTGRES_HOST"]
+
+
 TESTS_HOST = "51.250.8.141"
 # "62.84.117.31"
 TESTS_HOST_2 = "https://postgres-check-service.sprint9.tgcloudenv.ru"
 
 student = "avgredyaev"  # ваш_логин
 pg_settings = {
-    "host": "*",  # хост_вашего_postgresql
+    "host": host,  # хост_вашего_postgresql
     "port": 6432,  # порт_вашего_postgresql
-    "dbname": "*",  # название_бд
-    "username": "*",  # имя_пользователя_для_подключения
-    "password": "*",  # пароль_для_подключения
+    "dbname": "sprint9dwh",  # название_бд
+    "username": dbuser,  # имя_пользователя_для_подключения
+    "password": password,  # пароль_для_подключения
 }
 
 # укажите полный путь до папки с sprint-9-sample-service
