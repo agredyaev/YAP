4c09d443e8c2b72ebe31b6c9aaa53012f0a2f0dd
Aleksey Gredyaev <aleksey.gredyaev@gmail.com>
2023-02-18 15:29:42 +0100

    remove old one

--- s9-lessons/Тема 5. Написание первого сервиса/2. Проектируем структуру БД/Задание 4/create_cmd_user_category_counters.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE stg.order_events (
-	id serial NOT NULL,
-	object_id int NOT null UNIQUE,
-	object_type varchar NOT NULL,
-	sent_dttm timestamp NOT NULL,
-	payload json NOT NULL,
-	CONSTRAINT order_events_pk PRIMARY KEY (id)
-);
